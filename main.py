5a5d0b7bf739746d730fd4255a619f6faa3f2eab
Somvit09 <developer.09.sam@gmail.com>
2023-02-21 01:18:01 +0530

    added selenium method

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import pandas as pd
 import requests
 import bs4
 import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+driver_path = Service('chromedriver_linux64/chromedriver')
+driver = webdriver.Chrome(service=driver_path)
 
 
-def get_title_and_article_text():
+def get_title_and_article_text():  # by using beautifulsoup
     read_xl = pd.read_excel('Input.xlsx')
     data = {}
+    url_id = 37
+    all_links = []
+    urls = []
     for i in read_xl['URL']:
         response = requests.get(i)
         soup = bs4.BeautifulSoup(response.content, 'html.parser')
@@ -14,14 +23,41 @@ def get_title_and_article_text():
             title = soup.find('h1', class_='entry-title').text.strip()
             content = soup.find('div', class_='td-post-content').text
             data[title] = content.replace('\n', '')
+            all_links.append(i)
+            urls.append(url_id)
+            url_id += 1
         else:
-            pass
-    return data
+            url_id -= 1
+        print(len(all_links), len(urls), url_id, len(data))
+    return data, all_links, urls
+
+
+def get_title_and_article_text_by_selenium():  # by using selenium webdriver
+    read_xl = pd.read_excel('Input.xlsx')
+    data = {}
+    url_id = 37
+    all_links = []
+    urls = []
+    for i in read_xl['URL']:
+        driver.get(i)
+        if driver.find_element(By.CLASS_NAME, 'entry-title') is not None and driver.find_element(By.CLASS_NAME, 'td-post-content') is not None:
+            title = driver.find_element(By.CLASS_NAME, 'entry-title').text.strip()
+            content = driver.find_element(By.CLASS_NAME, 'td-post-content').text
+            data[title] = content.replace('\n', '')
+            all_links.append(i)
+            urls.append(url_id)
+            url_id += 1
+        else:
+            url_id -= 1
+        print(len(all_links), len(urls), url_id, len(data))
+        driver.quit()
+    return data, all_links, urls
 
 
 def store_data_to_excel_file():
+    data, all_links, urls = get_title_and_article_text()
     # create a Pandas dataframe from the dictionary
-    df = pd.DataFrame.from_dict(get_title_and_article_text(), orient='index', columns=['Value'])
+    df = pd.DataFrame.from_dict(data, orient='index', columns=['Value'])
 
     # create an Excel writer object
     writer = pd.ExcelWriter('data_collected_from_urls.xlsx')
@@ -124,7 +160,7 @@ def calculate_polarity_score():
     polarity_scores = []
     for i in range(len(positive_scores)):
         polarity_score = (positive_scores[i] - negative_scores[i]) / (
-                    (positive_scores[i] + negative_scores[i]) + 0.000001)
+                (positive_scores[i] + negative_scores[i]) + 0.000001)
         polarity_scores.append(polarity_score)
     return polarity_scores
 
@@ -175,13 +211,51 @@ def analysis_of_readability():
         no_of_words = len(all_articles[i].split(' '))
         no_of_sentences = len(all_articles[i].split('. '))
         complex_words = count_complex_words(all_articles[i])
-        average_length_of_sentence = (no_of_words/no_of_sentences)
-        complex_words_percentage = (complex_words/no_of_words)
+        average_length_of_sentence = (no_of_words / no_of_sentences)
+        complex_words_percentage = (complex_words / no_of_words)
         fog = (0.4 * (average_length_of_sentence + complex_words_percentage))
         average_sentence_length.append(average_length_of_sentence)
         percentage_of_complex_words.append(complex_words_percentage)
         fog_index.append(fog)
-    print(len(average_sentence_length), len(percentage_of_complex_words), len(fog_index))
     return average_sentence_length, percentage_of_complex_words, fog_index
 
-analysis_of_readability()
+
+def get_all_data_and_convert_it_into_excel_file():
+    data = {}
+    data_set, all_links, urls = get_title_and_article_text()
+    positive_list = positive_words_identification()
+    negative_list = negative_words_identification()
+    polarity_list = calculate_polarity_score()
+    subjective_list = calculate_subjectivity_score()
+    average_sentence_list, percentage_list, fog_list = analysis_of_readability()
+
+    # creating data
+    data['URL_ID'] = urls
+    data['URL'] = all_links
+    data['POSITIVE SCORE'] = positive_list
+    data['NEGATIVE SCORE'] = negative_list
+    data['POLARITY SCORE'] = polarity_list
+    data['SUBJECTIVITY SCORE'] = subjective_list
+    data['AVG SENTENCE LENGTH'] = average_sentence_list
+    data['PERCENTAGE OF COMPLEX WORDS'] = percentage_list
+    data['FOG INDEX'] = fog_list
+    print(len(all_links), len(urls), len(positive_list), len(negative_list), len(polarity_list), len(subjective_list),
+          len(average_sentence_list), len(percentage_list), len(fog_list))
+    # create a Pandas dataframe from the list
+    df = pd.DataFrame(data, columns=['URL_ID', 'URL', 'POSITIVE SCORE', 'NEGATIVE SCORE', 'POLARITY SCORE',
+                                     'SUBJECTIVITY SCORE', 'AVG SENTENCE LENGTH', 'PERCENTAGE OF COMPLEX WORDS',
+                                     'FOG INDEX'])
+
+    # create an Excel writer object
+    writer = pd.ExcelWriter('Output Data Structure.xlsx')
+
+    # write the dataframe to the Excel file
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+
+    # save the Excel file
+    writer.save()
+    writer.close()
+
+
+# get_all_data_and_convert_it_into_excel_file()
+get_title_and_article_text()
